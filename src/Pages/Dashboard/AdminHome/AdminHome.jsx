581316a708f270a506f7fc1fb871b69f17d52541
Md Mahbub Alom <mdmahbuba034@gmail.com>
2024-05-23 12:46:16 +0600

    admin home some item added

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,13 +1,77 @@
 import React from "react";
 import useAuth from "../../../hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaMoneyCheckAlt, FaTruck, FaUsers } from "react-icons/fa";
+import { PiChefHatBold } from "react-icons/pi";
 
 const AdminHome = () => {
   const { users } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: stats, isLoading } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("admin-stats");
+      return res.data;
+    },
+  });
+  if (isLoading) {
+    return <span>Loading......</span>;
+  }
 
   return (
     <div>
-      <span>Hi, Welcome </span>
-      <span>{users.displayName}</span>
+      <h2>
+        <span>Hi, Welcome </span>
+        {users?.displayName ? users?.displayName : "back"}
+      </h2>
+      <div className="stats shadow gap-2">
+        <div className="stat">
+          <div className="flex gap-2 items-center text-center">
+            <div className="text-4xl">
+              <FaMoneyCheckAlt />
+            </div>
+            <div>
+              <div className="stat-value">${stats?.revenue}</div>
+              <div className="stat-title">Revenue</div>
+            </div>
+          </div>
+        </div>
+        <div className="stat">
+          <div className="flex gap-2 items-center text-center">
+            <div className="text-4xl">
+              <FaUsers/>
+            </div>
+            <div>
+              <div className="stat-value">{stats?.users}</div>
+              <div className="stat-title">customers</div>
+            </div>
+          </div>
+        </div>
+        <div className="stat">
+          <div className="flex gap-2 items-center text-center">
+            <div className="text-4xl">
+            <PiChefHatBold />
+            </div>
+            <div>
+              <div className="stat-value">{stats?.menuItems}</div>
+              <div className="stat-title">Products</div>
+            </div>
+          </div>
+        </div>
+        <div className="stat">
+          <div className="flex gap-2 items-center text-center">
+            <div className="text-4xl">
+            <FaTruck />
+            </div>
+            <div>
+              <div className="stat-value">{stats?.orders}</div>
+              <div className="stat-title">Orders</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
